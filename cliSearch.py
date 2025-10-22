1c7198e6d5b3cacdc39df86200e24cbab1487aef
Aaditya Karamchandani <aad1tyak.me@gmail.com>
2025-10-22 13:58:15 -0400

    Created a search query

--- cliSearch.py
+++ cliSearch.py
@@ -1,3 +1,9 @@
+import argparse
 #Define the Arguement Parser object instance 
-parser = argparse.ArguementParser('cliSearch', 'Let the user to make search query through terminal.')
+parser = argparse.ArgumentParser('cliSearch', 'Let the user to make search query through terminal.')
 
+#attach arguements for task 
+parser.add_argument('query', nargs=1)
+
+args = parser.parse_args()
+print(args.query)
