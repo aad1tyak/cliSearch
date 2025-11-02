1a1c421c2f0fb8f67c17adba581c066da6290e19
Aaditya Karamchandani <aad1tyak.me@gmail.com>
2025-11-01 20:25:29 -0400

    Initiated gemini for simplifying the search result

--- cliSearch.py
+++ cliSearch.py
@@ -16,17 +16,27 @@ print(args.query)
 #google api key and cxid 
 API_KEY = os.environ.get("API_KEY")
 CX_ID = os.environ.get("CUSTOM_SEARCH_CX_ID")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 #url for search 
-url = "https://www.googleapis.com/customsearch/v1"
+search_url = "https://www.googleapis.com/customsearch/v1"
 
-params = {
+serach_params = {
         'key': API_KEY,
         'cx': CX_ID,
         'q': args.query,
         'num': 5  # Request up to 5 results for simplicity
     }
 
-response = requests.get(url, params)
+search_response = requests.get(search_url, search_params)
+
+#Using gemini to simplify the search query
+gemini_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+
+gemini_params = {
+    'key' = GEMINI_API_KEY
+}
+
+gemini_response = requests.get(gemini_url, gemini_params)
 
 print(response.json())
 
