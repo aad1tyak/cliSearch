e95c7706ad7a7075536aa1aa22825aab92b75afd
Aaditya Karamchandani <aad1tyak.me@gmail.com>
2025-11-02 18:27:54 -0500

    Finished with gemini flash implementation...still a little slow but I will try to fix it

--- cliSearch.py
+++ cliSearch.py
@@ -3,6 +3,9 @@ import os
 import json 
 import argparse
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.markdown import Markdown
+
 load_dotenv()
 #Define the Arguement Parser object instance 
 parser = argparse.ArgumentParser('cliSearch', 'Let the user to make search query through terminal.')
@@ -11,7 +14,6 @@ parser = argparse.ArgumentParser('cliSearch', 'Let the user to make search query
 parser.add_argument('query', nargs=1)
 
 args = parser.parse_args()
-print(args.query)
 
 #google api key and cxid 
 API_KEY = os.environ.get("API_KEY")
@@ -20,7 +22,7 @@ GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 #url for search 
 search_url = "https://www.googleapis.com/customsearch/v1"
 
-serach_params = {
+search_params = {
         'key': API_KEY,
         'cx': CX_ID,
         'q': args.query,
@@ -30,14 +32,24 @@ serach_params = {
 search_response = requests.get(search_url, search_params)
 
 #Using gemini to simplify the search query
-gemini_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+gemini_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + GEMINI_API_KEY
 
 gemini_params = {
-    'key' = GEMINI_API_KEY
+    "contents": [
+        {
+            "role": "user",
+            "parts": [
+                {
+                    "text": f"Summarize the key findings and main arguments from the following search results related. Present the answer in plain language, and address what the user is primarily seeking. Here is th search query: \n{args.query[0]}\nAnd here is the search result: \n{json.dumps(search_response.json())}"
+                }
+            ],
+        }
+    ]
 }
 
-gemini_response = requests.get(gemini_url, gemini_params)
 
-print(response.json())
+gemini_response = requests.post(gemini_url, json = gemini_params)
 
-print(API_KEY, CX_ID)
+console = Console()
+markdown = Markdown(gemini_response.json()['candidates'][0]['content']['parts'][0]['text'])
+console.print(markdown)
