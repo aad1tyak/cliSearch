416a39d7590d70eca503cd7de0e205aca3da45d9
Aaditya Karamchandani <aad1tyak.me@gmail.com>
2025-11-06 21:10:35 -0500

    Added the start and end in the query_search just to control each search sessions

--- query_search.py
+++ query_search.py
@@ -10,14 +10,19 @@ queryList_File = "QueryList.txt"
 
 search_results = [] #the input portion provided for training
 
+start = 0
+end = 500
+
 headers = {
   'X-API-KEY': os.environ.get("SERPER_DEV"),
   'Content-Type': 'application/json'
 }
-
+counter = 0
 def runScript(query):
+    counter += 1
     payload = json.dumps({
-        "q": query
+        "q": query,
+        "num": 4
     })
 
     try:
@@ -27,13 +32,17 @@ def runScript(query):
         clean_items = []
         for item in search_items:
             clean_items.append(f"Title: {item.get('title')}\n Snippet: {item.get('snippet')}\n Website Address:  {item.get('link')}\n \n \n")
-        search_results.append({"Query": query.strip(), "Search Items": "".join(clean_items)})
+        search_results.append({"Id: ": counter, Query": query.strip(), "Search Items": clean_items})
     except requests.exceptions.RequestException as e:
         print(f"Error during API request for query '{query.strip()}': {e}")
 
 #Go through the QueryList file and perfrom search for all of the query
 with open(queryList_File, "r", encoding='utf-8', errors="ignore") as f:
-    for line in f:
+    for i, line in enumerate(f):
+        if i < start:
+            continue
+        if i >= end:
+            break
         runScript(line)
 
 with open('query_response.csv', 'w', encoding='utf-8', newline='') as f:
@@ -41,3 +50,4 @@ with open('query_response.csv', 'w', encoding='utf-8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=search_results[0].keys())
         writer.writeheader()
         writer.writerows(search_results)
+
